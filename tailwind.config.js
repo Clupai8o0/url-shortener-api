6d90511dbca3bcbde435f7f4f9376765b7c77c95
Clupai8o0 <clupaio4@gmail.com>
2023-04-18 16:34:41 +0300

    html css completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
 	content: ["./src/**/*.{html,js}", "./index.html"],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ["Poppins", "Arial", "Helvetica", "sans-serif"],
+			},
+			colors: {
+				cyan: "hsl(180,66%,49%)",
+				"dark-violet": "hsl(257,27%,26%)",
+				red: "hsl(0,87%,67%)",
+				gray: "hsl(0,0%,75%)",
+				"grayish-violet": "hsl(257,7%,63%)",
+				"very-dark-blue": "hsl(255,11%,22%)",
+				"very-dark-violet": "hsl(260,8%,14%)",
+				'bg-gray': 'hsl(0,0%,95%)'
+			},
+		},
 	},
 	plugins: [],
 };
