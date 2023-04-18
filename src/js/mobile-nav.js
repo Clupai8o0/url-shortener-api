119e8cd4118346c2f1a7bdf7b26ebd2eda0e398e
Clupai8o0 <clupaio4@gmail.com>
2023-04-18 20:53:51 +0300

    mobile nav animations

--- src/js/mobile-nav.js
+++ src/js/mobile-nav.js
@@ -1,21 +1,28 @@
-const burger = document.getElementById("burger");
+const checkbox = document.getElementById("checkbox");
+const mobileNavOverlay = document.getElementById("nav-mobile-overlay");
 const mobileNav = document.getElementById("nav-mobile");
 
 let open = false;
 
-function showMobileNav() {
-  mobileNav.classList.remove("hidden")
-  open = true
+function openMobileNav() {
+	mobileNavOverlay.classList.remove("hidden");
+	mobileNavOverlay.classList.remove("fade-out-top");
+	mobileNav.classList.remove("fade-in-top");
+	mobileNav.classList.add("fade-in-top")
+	open = true;
 }
 
 function closeMobileNav() {
-	mobileNav.classList.add("hidden");
+	mobileNav.classList.remove("fade-in-top");
+	mobileNavOverlay.classList.remove("fade-out-top");
+	mobileNavOverlay.classList.add("fade-out-top");
 	open = false;
+	checkbox.checked = false;
 }
 
+checkbox.addEventListener("click", () => {
+	if (!open) openMobileNav();
+	else closeMobileNav();
+});
 
-burger.addEventListener("click", () => {
-	console.log("hi")
-  if (!open) showMobileNav()
-	else closeMobileNav()
-})
+mobileNavOverlay.addEventListener("click", closeMobileNav);
